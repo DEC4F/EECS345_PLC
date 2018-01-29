149f2a869a5ea73b460dbba5ab85aaa9361d8355
dec4f <rarunok@yahoo.com>
2018-01-29 00:57:49 -0500

    added solution to Q5, half way done with Q6 Q7

--- Programming_Exercise/PE1.scm
+++ Programming_Exercise/PE1.scm
@@ -39,10 +39,53 @@ Stanley
 ; 5. deepcons takes an element and a list, that possibly containst sublists and places the element in the front of the first element, as deep in the sublist as needed
 (define deepcons
   (lambda (x lst)
+    (cond
+      ((null? (car lst)) (cons x (cdr lst)))                      ; edge case, append x to the rest if the head of curr list is null,
+      ((pair? (car lst)) (cons (deepcons x (car lst)) (cdr lst))) ; recursive case, append result of deepcons of x and curr head (sublist) with rest
+      (else (cons x lst))                                         ; base case, append x to the list
+    )
   ))
 
+; 6. numparens takes a list and returns the number of pairs of parentheses 
+;TODO not done yet
+(define numparens
+  (lambda (lst)
+    (cond
+      ((null? lst) 1) 
+      ((pair? (car lst)) 1 + (numparens (car lst)) + (numparens (cdr lst))) 
+      (else 0 + (numparens (cdr lst))) ))) 
 
+; base case, list end, return 1 as the pren of the input list
+; recursive case, add 1 to the output of curr sublist and the rest
+; recursive case, add 0 to the output of the rest 
 
+; 7. dup* takes a list and duplicates all contents, including any sublists 
+;TODO not done yet
+(define dup* 
+  (lambda (lst) 
+    (cond 
+      ((null? lst) '()) 
+      ((pair? car lst) (cons (list (dup* (car lst)) (dup* (car lst)) ) (dup* (cdr lst)))) 
+      (else (cons (list (car lst) (car lst)) (dup* (cdr lst)))) )))
 
+; base case, reached the list end
+; recursive case, dup the sublist itself and recursive dup its element and append to dup of rest
+; recursive case, dup curr head and append to result of the rest
 
+; 8. removedups* takes a list, that can contain sublists, and removes any atom that is the repeat of the atom that immediately precedes it in the same sublist
+(define removedups* 
+  (lambda (lst) 
+    (cond 
+    )))
 
+; 9. split* takes a list, that can contain sublists, and returns a list containing two lists. The first list should contain the 1st, 3rd, 5th, ... elements, and the second list should contain the 2nd, 4th, 6th, ... elements. However, if any of these elements are also lists, these elements should be split as well.
+(define split* 
+  (lambda (lst) 
+    (cond 
+    )))
+
+; 10. removedups** takes a list, that can contain sublists, and removes any element that, once repeated elements have been removed from it, is the repeat of any element (also once elements have been removed from it) that immediately precedes it in the same sublist
+(define removedups**
+  (lambda (lst) 
+    (cond 
+    )))
