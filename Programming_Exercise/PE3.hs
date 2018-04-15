04274bc0aad352e684a09f831c5eb713f3900860
DEC4F <dec4f@localhost.localdomain>
2018-04-15 18:36:12 -0400

    replaced if statement with guards

--- Programming_Exercise/PE3.hs
+++ Programming_Exercise/PE3.hs
@@ -1,35 +1,29 @@
-{- takes a list and removes duplicate elements. -}
+{- 1. takes a list and removes duplicate elements. -}
 removedups :: [Int] -> [Int]
 removedups [a] = [a]
-removedups lst =
-  if (head lst) == (head . tail) lst
-    then
-      removedups (tail lst)
-    else
-      (head lst) : removedups (tail lst)
+removedups lst
+  | (head lst) == (head . tail) lst = removedups (tail lst)
+  | otherwise                       = (head lst) : removedups (tail lst)
 
-{- Create a continuation passing version, removedups-cps -}
+{- 2. Create a continuation passing version, removedups-cps -}
 removedups_cps :: [Int] -> ([Int] -> p) -> p
 removedups_cps [a] return = return [a]
-removedups_cps lst return =
-  if (head lst) == (head . tail) lst
-    then
-      removedups_cps (tail lst) return
-    else
-      removedups_cps (tail lst) (\v -> return ((head lst) : v))
+removedups_cps lst return
+  | (head lst) == (head . tail) lst = removedups_cps (tail lst) return
+  | otherwise                       = removedups_cps (tail lst) (\v -> return ((head lst) : v))
 
-{- Create a type that allows us to have nested lists. Your type should have two kinds of values, elements and sublists -}
+{- 3. Create a type that allows us to have nested lists. Your type should have two kinds of values, elements and sublists -}
+-- data StarList s = Nothing | Element Int | Sublist s [StarList s] deriving (Show)
 data StarList s = Element Int | Sublist s deriving (Show)
+gettype (Element a) = "Element"
+gettype (Sublist a) = "Sublist"
 
-{- Create the function gremovedups that takes a list containing elements and sublists and returns a list with the same structure, but if any "element" is preceded by an identical element, that element is removed 
+{- 4. Create the function gremovedups that takes a list containing elements and sublists and returns a list with the same structure, but if any "element" is preceded by an identical element, that element is removed -}
 gremovedups :: [StarList] -> [StarList]
 gremovedups [a] = [a]
-gremovedups lst =
-  if :t (head lst) == Sublist -- is a sublist/not element
-    then
-      gremovedups (head lst) : gremovedups (tail lst)
-    else if (head lst) == (head . tail) lst
-      then
-        gremovedups (tail lst)
-      else
-        (head lst) : gremovedups (tail lst) -}
+gremovedups lst
+  | (gettype . head) lst /= "Element" = gremovedups (head lst) : gremovedups (tail lst)
+  | (head lst) == (haed . tail) lst   = gremovedups (tail lst)
+  | otherwise                         = (head lst) : gremovedups (tail lst)
+
+{- 5.  -}
