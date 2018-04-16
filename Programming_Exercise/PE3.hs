56461bc826267762eef4701f5a9464ae18a1abc3
dec4f <rarunok@yahoo.com>
2018-04-16 01:36:15 -0400

    solved P3 & 4

--- Programming_Exercise/PE3.hs
+++ Programming_Exercise/PE3.hs
@@ -2,28 +2,31 @@
 removedups :: [Int] -> [Int]
 removedups [a] = [a]
 removedups lst
-  | (head lst) == (head . tail) lst = removedups (tail lst)
-  | otherwise                       = (head lst) : removedups (tail lst)
+  | head lst == (head . tail) lst = removedups (tail lst)
+  | otherwise                     = (head lst) : removedups (tail lst)
 
 {- 2. Create a continuation passing version, removedups-cps -}
 removedups_cps :: [Int] -> ([Int] -> p) -> p
 removedups_cps [a] return = return [a]
 removedups_cps lst return
-  | (head lst) == (head . tail) lst = removedups_cps (tail lst) return
-  | otherwise                       = removedups_cps (tail lst) (\v -> return ((head lst) : v))
+  | head lst == (head . tail) lst = removedups_cps (tail lst) return
+  | otherwise                     = removedups_cps (tail lst) (\v -> return ((head lst) : v))
 
 {- 3. Create a type that allows us to have nested lists. Your type should have two kinds of values, elements and sublists -}
--- data StarList s = Nothing | Element Int | Sublist s [StarList s] deriving (Show)
-data StarList s = Element Int | Sublist s deriving (Show)
-gettype (Element a) = "Element"
-gettype (Sublist a) = "Sublist"
+data Starlist s = Element Int | Sublist [Starlist s] deriving (Eq, Show)
 
 {- 4. Create the function gremovedups that takes a list containing elements and sublists and returns a list with the same structure, but if any "element" is preceded by an identical element, that element is removed -}
-gremovedups :: [StarList] -> [StarList]
-gremovedups [a] = [a]
+gremovedups :: [Starlist a] -> [Starlist a]
+gremovedups [] = []
+gremovedups [Element a] = [Element a] 
+gremovedups [Sublist a] = [Sublist (gremovedups a)] -- can't reach inside the sublist
 gremovedups lst
-  | (gettype . head) lst /= "Element" = gremovedups (head lst) : gremovedups (tail lst)
-  | (head lst) == (haed . tail) lst   = gremovedups (tail lst)
-  | otherwise                         = (head lst) : gremovedups (tail lst)
+  | head lst == (head . tail) lst = gremovedups (tail lst)
+  | otherwise                     = (head lst) : (gremovedups .tail) lst
+ 
+{- 5. Using the Tree type created in class (use a version without lists), write a function bubbledown that takes a Tree as input. If the element stored in the root is larger than either children, swap the element with the smaller child, and recurse on the child you swapped the element with. The recursion should stop when either you reach a leaf or when the element of the node is smaller than both its children. -}
+data Tree t = Leaf t | Internal t (Tree t) (Tree t) deriving (Show)
 
-{- 5.  -}
+bubbledown :: (Tree a) -> (Tree a)
+bubbledown (Leaf a) = (Leaf a)
+bubbledown (Internal a) = (Internal )
