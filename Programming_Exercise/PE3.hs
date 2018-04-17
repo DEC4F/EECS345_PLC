35097d496dccdd7bae11eb6d03cd374ca2e5f9fb
DEC4F <dec4f@localhost.localdomain>
2018-04-16 21:53:13 -0400

    solved all except P5

--- Programming_Exercise/PE3.hs
+++ Programming_Exercise/PE3.hs
@@ -1,32 +1,68 @@
-{- 1. takes a list and removes duplicate elements. -}
+{- 1. takes a lis-t and removes duplicate elements. -}
 removedups :: [Int] -> [Int]
 removedups [a] = [a]
 removedups lst
-  | head lst == (head . tail) lst = removedups (tail lst)
-  | otherwise                     = (head lst) : removedups (tail lst)
+  | head lst == (head.tail) lst = removedups (tail lst)
+  | otherwise                   = (head lst) : removedups (tail lst)
 
 {- 2. Create a continuation passing version, removedups-cps -}
 removedups_cps :: [Int] -> ([Int] -> p) -> p
 removedups_cps [a] return = return [a]
 removedups_cps lst return
-  | head lst == (head . tail) lst = removedups_cps (tail lst) return
-  | otherwise                     = removedups_cps (tail lst) (\v -> return ((head lst) : v))
+  | head lst == (head.tail) lst = removedups_cps (tail lst) return
+  | otherwise                   = removedups_cps (tail lst) (\v -> return ((head lst) : v))
 
 {- 3. Create a type that allows us to have nested lists. Your type should have two kinds of values, elements and sublists -}
-data Starlist s = Element Int | Sublist [Starlist s] deriving (Eq, Show)
+data Starlist s = Element s | Sublist [Starlist s] deriving (Show)
+instance (Eq s) => Eq (Starlist s) where
+  (Element x) == (Element y) = x == y
+  (Element x) == (Sublist y) = False
+  (Sublist x) == (Element y) = False
+  (Sublist x) == (Sublist y) = False
 
 {- 4. Create the function gremovedups that takes a list containing elements and sublists and returns a list with the same structure, but if any "element" is preceded by an identical element, that element is removed -}
-gremovedups :: [Starlist a] -> [Starlist a]
 gremovedups [] = []
-gremovedups [Element a] = [Element a] 
-gremovedups [Sublist a] = [Sublist (gremovedups a)] -- can't reach inside the sublist
+gremovedups [Element a] = [Element a]
+gremovedups [Sublist a] = [Sublist (gremovedups a)]
 gremovedups lst
-  | head lst == (head . tail) lst = gremovedups (tail lst)
-  | otherwise                     = (head lst) : (gremovedups .tail) lst
+  | head lst == head (tail lst) = gremovedups (tail lst)
+  | otherwise                   = gremovedups ([(head lst)]) ++ (gremovedups (tail lst))
  
 {- 5. Using the Tree type created in class (use a version without lists), write a function bubbledown that takes a Tree as input. If the element stored in the root is larger than either children, swap the element with the smaller child, and recurse on the child you swapped the element with. The recursion should stop when either you reach a leaf or when the element of the node is smaller than both its children. -}
-data Tree t = Leaf t | Internal t (Tree t) (Tree t) deriving (Show)
+data Tree t = Leaf t | Internal t (Tree t) (Tree t) deriving (Eq, Show)
+-- bubbledown :: (Tree a) -> [Tree a]
+bubbledown (Leaf a) = [a]
+bubbledown (Internal a l r)
+  | a > l && a > r && l < r = (l : (bubbledown a)) ++ (bubbledown r)
+  | a > l && a > r && r < l = (r : (bubbledown l)) ++ (bubbledown a)
+  | a > l && a < r          = (l : (bubbledown a)) ++ (bubbledown r)
+  | a > r && a < l          = (r : (bubbledown l)) ++ (bubbledown a)
+  | otherwise               = (a : (bubbledown l)) ++ (bubbledown r)
 
-bubbledown :: (Tree a) -> (Tree a)
-bubbledown (Leaf a) = (Leaf a)
-bubbledown (Internal a) = (Internal )
+{- 6. Using the Maybe monad of Haskell, create a function called checkcons that has the following type. The function takes a Maybe value of some type, a Maybe list of the same type (as a monad), and a test function and returns a Maybe list of the same type. If either Maybe is Nothing, the result is Nothing. If the first Maybe value passes the test function, the result has the first element cons'd onto the front of the list. Otherwise the result is Nothing. -}
+checkcons :: Maybe a -> Maybe [a] -> (a -> Bool) -> Maybe [a]
+checkcons Nothing _ _ = Nothing
+checkcons mx mlist test = do
+  x   <- mx
+  lst <- mlist
+  if (test x)
+    then return (x : lst)
+    else Nothing
+
+{- 7. Using checkcons create a function checklist that takes a list and a function and returns Nothing if the elements in the list fail to past the function and the list (embedded in a Maybe) if all the elements pass. -}
+checklist :: [a] -> (a -> Bool) -> Maybe [a]
+checklist [] _ = Just []
+checklist lst test = checkcons (Just (head lst)) (checklist (tail lst) test) test
+
+{- 8. Create a list monad that generalizes a list. This will not be a Haskell Monad type, but instead one of our own creation like the Value type from lecture. -}
+data List a = Null | Pair a (List a) deriving (Eq, Show)
+{- Then create a binding function lbind and a return function lreturn to make a list monad. -}
+lreturn a = Pair a Null
+
+lbind :: List a -> (a -> List a1) -> List a1
+lbind Null _       = Null
+lbind (Pair a Null) f = (f a)
+lbind (Pair a l) f = lcons (f a) (lbind l f)
+
+-- helper method that get rid of the extra Null
+lcons (Pair a b) (Pair c d) = Pair a (Pair c d)
